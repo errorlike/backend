895e27bc6a76fd75e49da2c77ef4da42d1fe980c
errorlike <errorlikeman@outlook.com>
2022-10-06 01:51:27 +0800

    exercise4.1-4.2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,20 @@
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogRouter = require('./controllers/blogs');
+const mongoose = require('mongoose')
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+mongoose.connect(config.MONGO_URL).then(() => {
+    logger.info('mongo connect');
+}).catch(error => {
+    logger.error('error connecting to MongoDB:', error.message);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'))
+
+app.use('/api/blogs',blogRouter);
+module.exports = app;
---
+++ part4/blog-list/utils/config.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGODB_URL;
+module.exports = { PORT, MONGO_URL };
